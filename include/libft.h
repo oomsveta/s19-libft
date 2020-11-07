d57be172dde3d1d0ddc38c096550bf625b7f080f
Oom Sveta <oomsveta@protonmail.com>
2020-11-07 13:14:10 +0100

    Create libft.h

---
+++ include/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <lwicket@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 1970/01/01 00:00:00 by lwicket           #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+typedef char				t_i8;
+typedef unsigned char		t_u8;
+typedef short int			t_i16;
+typedef unsigned short int	t_u16;
+typedef int					t_i32;
+typedef unsigned int		t_u32;
+
+void	ft_putstr(char *str);
+
+#endif
