5bee4d757845d468b5bb39c72562f6b9b04ca883
Oom Sveta <oomsveta@protonmail.com>
2020-11-16 11:39:42 +0100

    Rename 'ft_strcat' to 'lw_strcat'

--- include/libft.h
+++ include/libft.h
@@ -47,10 +47,10 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
-char	*ft_strcat(char *dest, const char *src);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
+char	*lw_strcat(char *dest, const char *src);
 
 /*
 **	CTYPE
