9bd4019df8f2c31ed12f47b25488db73b3605356
Oom Sveta <oomsveta@protonmail.com>
2020-11-16 13:43:08 +0100

    Add ft_strncmp

--- include/libft.h
+++ include/libft.h
@@ -47,6 +47,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
