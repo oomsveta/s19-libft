c50308383a0c1e6cb86561e6b2c3727841cbb376
Oom Sveta <oomsveta@protonmail.com>
2020-11-16 16:24:56 +0100

    Add ft_memcmp

---
+++ src/string/ft_memcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <lwicket@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 1970/01/01 00:00:00 by lwicket           #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const t_u8	*ptr1;
+	const t_u8	*ptr2;
+
+	ptr1 = s1;
+	ptr2 = s2;
+	while (n--)
+		if (*ptr1++ != *ptr2++)
+			return (*--ptr1 - *--ptr2);
+	return (0);
+}
