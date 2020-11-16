bc06a9e207ba1a08c26dc698a74cd8ad04de970b
Oom Sveta <oomsveta@protonmail.com>
2020-11-16 14:27:05 +0100

    Add ft_isprint

--- include/libft.h
+++ include/libft.h
@@ -61,6 +61,7 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_isprint(int c);
 t_u8	lw_isspace(int c);
 
 /*
---
+++ src/ctype/ft_isprint.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <lwicket@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 1970/01/01 00:00:00 by lwicket           #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Tests if the provided character is printable.
+**
+**	@param c	A single character.
+**	@returns	1 if the character is printable, otherwise 0.
+*/
+
+int	ft_isprint(int c)
+{
+	return (c > 31 && c != 127);
+}
