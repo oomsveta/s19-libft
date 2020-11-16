041d5b36cc106cd9feb17b4d70af5e6f8a3af1b3
Oom Sveta <oomsveta@protonmail.com>
2020-11-16 09:45:34 +0100

    Add ft_strlcat.c

---
+++ src/string/ft_strlcat.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <lwicket@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 1970/01/01 00:00:00 by lwicket           #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Appends the null-terminated string src to the end of dst.
+**
+**	@param dst	The destination string.
+**	@param src	The source string.
+**	@param size	The length of the string to create.
+**	@returns	The length of the string it tried to create.
+*/
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	const char *d;
+	const char *s;
+	size_t len;
+	size_t n;
+
+	d = dst;
+	s = src;
+	while (n-- && *d)
+		d++;
+	len = dst - d;
+	if (!n--)
+		return (len + ft_strlen(src));
+	while (*src)
+	{
+		if (n)
+		{
+			*dest++ = *src;
+			n--;
+		}
+		src++;
+	}
+	*dest = 0;
+	return (len + (src - s));
+}
