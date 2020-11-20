0d4d03957f9d2c2e94c6bc27c76aa2b7e02348f6
lwicket <oomsveta@protonmail.com>
2020-11-20 10:47:50 +0100

    Fix: memmove does not crash anymore when both params are NULL

--- src/string/ft_memmove.c
+++ src/string/ft_memmove.c
@@ -12,11 +12,24 @@
 
 #include "libft.h"
 
+/*
+**	Copies n bytes from memory area src to memory area dest. The memory areas
+**	may overlap: copying takes place as though the bytes in src are first
+**	copied into a temporary array that does not overlap src or dest, and the
+**	bytes are then copied from the temporary array to dest.
+**
+**	@param dest	The memory area to copy into.
+**	@param src	The memory area to copy from.
+**	@param n	The number of bytes to copy.
+*/
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	t_u8		*d;
 	const t_u8	*s;
 
+	if (dest == src)
+		return (dest);
 	d = dest;
 	s = src;
 	if (s < d)
