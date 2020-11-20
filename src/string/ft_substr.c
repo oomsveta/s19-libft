2a25adb86b9e50b9a40c864828667b1a4952e93d
lwicket <oomsveta@protonmail.com>
2020-11-20 16:04:45 +0100

    Fix: Return empty string instead of NULL when start > strlen

--- src/string/ft_substr.c
+++ src/string/ft_substr.c
@@ -12,18 +12,28 @@
 
 #include "libft.h"
 
+/*
+**	Allocates and returns a substring from the string 's' beginning at index
+**	'start' and of maximun size 'len'.
+**
+**	@param s		The string from which to create the substring.
+**	@param start	The start index of the substring in the string 's'.
+**	@param len		The maximum length of the substring.
+*/
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ret;
 	char	*p;
 	int		i;
 
-	if (start > ft_strlen(s) || !(ret = malloc(len + 1)))
+	if (!(ret = malloc(len + 1)))
 		return (NULL);
 	p = (char *)s + start;
 	i = 0;
-	while (*p && len--)
-		ret[i++] = *p++;
+	if (start < ft_strlen(s))
+		while (*p && len--)
+			ret[i++] = *p++;
 	ret[i] = '\0';
 	return (ret);
 }
