b139c180dc166d336d37157554a619ee425309fb
lwicket <oomsveta@protonmail.com>
2020-11-20 14:44:05 +0100

    Add new integer types

--- include/libft.h
+++ include/libft.h
@@ -19,12 +19,14 @@
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
 
-typedef char				t_i8;
-typedef unsigned char		t_u8;
-typedef short int			t_i16;
-typedef unsigned short int	t_u16;
-typedef int					t_i32;
-typedef unsigned int		t_u32;
+typedef char					t_i8;
+typedef unsigned char			t_u8;
+typedef short int				t_i16;
+typedef unsigned short int		t_u16;
+typedef int						t_i32;
+typedef unsigned int			t_u32;
+typedef long long int			t_i64;
+typedef unsigned long long int	t_u64;
 
 typedef struct	s_list
 {
