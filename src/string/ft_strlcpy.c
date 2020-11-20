bc777d4d66898d8b73810d72c5b528c9bd7719a8
lwicket <oomsveta@protonmail.com>
2020-11-20 13:16:34 +0100

    Add ft_strlcpy

---
+++ src/string/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwicket <lwicket@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 1970/01/01 00:00:00 by lwicket           #+#    #+#             */
+/*   Updated: 1970/01/01 00:00:00 by lwicket          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	const char	*start;
+	size_t		n;
+
+	start = src;
+	n = dstsize;
+	if (n)
+		while (--n)
+			if (!(*dst++ = *src++))
+				break ;
+	if (!n)
+	{
+		if (dstsize)
+			*dst = '\0';
+		while (*src++)
+			;
+	}
+	return (src - start - 1);
+}
