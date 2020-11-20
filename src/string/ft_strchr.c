b5307dffa0f1f8bc3137ff4c8013bdfb93441304
lwicket <oomsveta@protonmail.com>
2020-11-20 10:59:48 +0100

    Fix: If the character to find is '\0', return the a pointer to the terminator instead of NULL

--- src/string/ft_strchr.c
+++ src/string/ft_strchr.c
@@ -12,10 +12,21 @@
 
 #include "libft.h"
 
+/*
+**	Finds the first occurrence of the character 'c' in the string 's'.
+**
+**	@param s	The string to search into.
+**	@param c	The character to search for.
+**	@returns	A pointer to the matched character or NULL if the character is
+**				not found. The terminating null byte is considered part of the
+**				string, so that if c is specified as '\0', returns a pointer to
+**				the terminator.
+*/
+
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 		if (*s++ == c)
 			return ((char *)--s);
-	return (NULL);
+	return (c ? NULL : (char *)s);
 }
