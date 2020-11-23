85da9a9703250ca094de168202ea24f05a57a40e
lwicket <oomsveta@protonmail.com>
2020-11-23 13:24:17 +0100

    Handle the case where lst is empty

--- src/linked_list/ft_lstadd_back.c
+++ src/linked_list/ft_lstadd_back.c
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (!lst || !new)
+	if (!new || !lst)
 		return ;
-	ft_lstlast(*lst)->next = new;
+	*lst ? (ft_lstlast(*lst)->next = new) : (*lst = new);
 }
