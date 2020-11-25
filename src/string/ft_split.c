c31ab73325ce5d4d4e0f4b3e4e44dbfb0eab020b
Oom Sveta <oomsveta@protonmail.com>
2020-11-25 10:26:14 +0100

    Finally fix split? (svp)

--- src/string/ft_split.c
+++ src/string/ft_split.c
@@ -16,10 +16,6 @@ static size_t	count_words(const char *s, char c)
 {
 	size_t ret;
 
-	if (!s)
-		return (0);
-	if (!*s)
-		return (1);
 	ret = 0;
 	while (*s)
 		if (*s++ != c)
@@ -45,23 +41,24 @@ char			**ft_split(char const *s, char c)
 	int			i;
 	size_t		len;
 
+	if (!s)
+		return (NULL);
 	len = count_words(s, c);
 	if (!(ret = malloc(sizeof(char *) * (len + 1))))
 		return (NULL);
 	ret[len] = NULL;
 	i = 0;
-	if (s)
-		while (*s)
+	while (*s)
+	{
+		start = s;
+		if (*s++ != c)
 		{
-			start = s;
-			if (*s++ != c)
-			{
-				while (*s != c && *s)
-					s++;
-				if (!(ret[i] = ft_substr(start, 0, s - start)))
-					return (free_all(ret, i));
-				i++;
-			}
+			while (*s != c && *s)
+				s++;
+			if (!(ret[i] = ft_substr(start, 0, s - start)))
+				return (free_all(ret, i));
+			i++;
 		}
+	}
 	return (ret);
 }
