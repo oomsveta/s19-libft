8ce6959c8da553ba8afefed8efca7ba9eed88b6a
Oom Sveta <oomsveta@protonmail.com>
2021-01-11 15:10:26 +0100

    Refactor

--- src/vector/u8_vec_cat.c
+++ src/vector/u8_vec_cat.c
@@ -16,11 +16,6 @@ int		u8_vec_cat(t_u8_vec *vec, char *str)
 {
 	if (str)
 		while (*str)
-		{
-			if (vec->length + 1 > vec->capacity)
-				if (!u8_vec_resize(vec, vec->capacity * 2))
-					return (0);
-			vec->content[vec->length++] = *str;
-		}
+			u8_vec_push(vec, *str);
 	return (1);
 }
